d4d53c34ce6188318207167ef85944ecd0ad32e2
alireza mirzaei <alirezamirzaei25@gmail.com>
2024-01-02 00:21:12 +0000

    fix(schema): fix type options

--- src/schemas/transaction.schema.ts
+++ src/schemas/transaction.schema.ts
@@ -1,19 +1,16 @@
 import * as mongoose from 'mongoose';
-import { IsDate, IsString, Min } from 'class-validator';
 import { TransactionStatus, TransactionType } from '../constants';
 
 export const TransactionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  date: { type: Date, required: true, validate: { validator: IsDate } },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+  date: { type: Date, required: true },
   fromAccountNumber: {
     type: String,
     required: true,
-    validate: { validator: IsString },
   },
   toAccountNumber: {
     type: String,
     required: false,
-    validate: { validator: IsString },
   },
   status: {
     type: String,
@@ -28,7 +25,37 @@ export const TransactionSchema = new mongoose.Schema({
   amount: {
     type: Number,
     required: true,
-    validate: { validator: Min(0) },
     default: 0,
   },
 });
+
+// export const TransactionSchema = new mongoose.Schema({
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+//   date: { type: Date, required: true, validate: { validator: IsDate } },
+//   fromAccountNumber: {
+//     type: String,
+//     required: true,
+//     validate: { validator: IsString },
+//   },
+//   toAccountNumber: {
+//     type: String,
+//     required: false,
+//     validate: { validator: IsString },
+//   },
+//   status: {
+//     type: String,
+//     enum: Object.values(TransactionStatus),
+//     default: TransactionStatus.CREATED,
+//   },
+//   type: {
+//     type: String,
+//     enum: Object.values(TransactionType),
+//     default: TransactionType.WITHDRAW,
+//   },
+//   amount: {
+//     type: Number,
+//     required: true,
+//     validate: { validator: Min(0) },
+//     default: 0,
+//   },
+// });
